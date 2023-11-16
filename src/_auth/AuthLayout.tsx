c4a5b48dc38807f4115541de088c406d629382e6
John Kinya <kingjohnkn@gmail.com>
2023-11-16 18:18:48 +0300

    sign up page

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -9,9 +9,14 @@ const AuthLayout = () => {
         <Navigate to="/" />
       ) : (
         <>
-          <section>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden sm:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
         </>
       )}
     </>
