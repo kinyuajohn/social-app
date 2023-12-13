5089ece5cccae95b0d2f5d59940fcd2621f70746
John Kinya <kingjohnkn@gmail.com>
2023-12-13 15:02:18 +0300

    sign up form

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -14,30 +14,41 @@ import { Input } from "@/components/ui/input";
 
 import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
-
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-});
+import { SignupValidation } from "@/lib/validation";
 
 const SignupForm = () => {
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
     defaultValues: {
+      name: "",
       username: "",
+      email: "",
+      password: "",
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
   return (
-    <div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+    <Form {...form}>
+      <div className="flex-center flex-col">
+        <img src="/assets/images/logo.svg" alt="" />
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Create a new account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          To use MyGram, enter your details
+        </p>
+
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-5 w-full mt-4"
+        >
           <FormField
             control={form.control}
             name="username"
@@ -56,8 +67,8 @@ const SignupForm = () => {
           />
           <Button type="submit">Submit</Button>
         </form>
-      </Form>
-    </div>
+      </div>
+    </Form>
   );
 };
 
