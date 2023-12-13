30341f929d8d95bda7e0b47aea4cee948bfc1835
John Kinya <kingjohnkn@gmail.com>
2023-12-13 16:17:04 +0300

    appwrite start

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -32,10 +32,8 @@ const SignupForm = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    // const newUser = await createUserAccount(values);
   }
   return (
     <Form {...form}>
---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,5 @@
+import { Client, Account, Databases, Storage, Avatars } from "appwrite";
+
+export const appwriteConfig = {
+  projectId: "6555e5bb2d713d0fd15e",
+};
