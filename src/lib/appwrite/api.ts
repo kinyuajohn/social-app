f05b3798ddd97f9207b1e1727aa7e1768635f69d
John Kinya <kingjohnkn@gmail.com>
2023-12-15 00:09:07 +0300

    postcard

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -151,7 +151,7 @@ export async function uploadFile(file: File) {
   }
 }
 
-export async function getFilePreview(fileId: string) {
+export function getFilePreview(fileId: string) {
   try {
     const fileUrl = storage.getFilePreview(
       appwriteConfig.storageId,
