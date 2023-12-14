d0e2c90dac10dd16d0404b0d7532a0fdbf29ca0f
John Kinya <kingjohnkn@gmail.com>
2023-12-14 16:30:35 +0300

    bottom bar

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { bottombarLinks } from "@/constants";
+import { Link, useLocation } from "react-router-dom";
 
 const Bottombar = () => {
-  return <div>Bottombar</div>;
+  const { pathname } = useLocation();
+  return (
+    <section className="bottom-bar">
+      {bottombarLinks.map((link) => {
+        const isActive = pathname === link.route;
+
+        return (
+          <Link
+            to={link.route}
+            key={link.label}
+            className={`leftsidebar-link group ${
+              isActive && "bg-primary-500 rounded-[10px]"
+            } flex-center flex-col gap-1 p-2 transition`}
+          >
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              width={16}
+              height={16}
+              className={`group-hover:invert-white ${
+                isActive && "invert-white"
+              }`}
+            />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Bottombar;
