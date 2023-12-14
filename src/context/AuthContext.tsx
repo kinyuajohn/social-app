f336b9b74be762057a04ac9fe3d199c85ff8267c
John Kinya <kingjohnkn@gmail.com>
2023-12-14 13:39:53 +0300

    sign in and sign up forms

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -59,8 +59,11 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   };
 
   useEffect(() => {
-    // localStorage.getItem("cookieFallback") === null
-    if (localStorage.getItem("cookieFallback") === "[]") navigate("/sign-in");
+    if (
+      localStorage.getItem("cookieFallback") === "[]" ||
+      localStorage.getItem("cookieFallback") === null
+    )
+      navigate("/sign-in");
 
     checkAuthUser();
   }, []);
